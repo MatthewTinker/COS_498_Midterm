a36dd49d4b2f3657ee845255c4d0363a1d3eadc3
MatthewTinker <matthew.tinkham@maine.edu>
2025-11-05 12:36:14 -0500

    Conditional

--- nodejs/server.js
+++ nodejs/server.js
@@ -20,8 +20,47 @@ app.use(express.json());
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 
-// Remove static file serving - nginx will handle this
-// app.use(express.static('public')); // Remove this line
+hbs.registerHelper('formatDate', function(date) {
+    return date.toLocaleString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+        hour : 'numeric',
+        minute: '2-digit'
+    })
+})
+
+
+//Create cookies
+app.use((req, _res, next) => {
+    const sessionId = req.cookies.sessionId;
+
+    if (sessionId && sessions[sessionId]) {
+        const session = sessions[sessionId];
+
+        if (session.expires > new Date()) {
+            req.user = session.user;
+        }
+
+    }
+
+    next()
+})
+
+let sessionCounter = 0;
+
+function createSession(username) {
+    const sessionId = sessionCounter.toString();
+    sessions[sessionId] = {
+        user: username,
+        sessionId: sessionId,
+        expires: new Date(Date.now() + 3600000)
+    };
+
+    sessionCounter++;
+    return sessionId;
+}
+
 
 // API Routes
 // Note: We don't include '/api' in our routes because nginx strips it when forwarding
@@ -34,29 +73,56 @@ app.get('/', (req, res) => {
     });
 });
 
+
+
+//App Get requests
 app.get('/home', (req, res) => {
-    res.render('home');
+    res.render('home', { title: "Home", user: req.user || null, year: new Date().getFullYear() });
 });
 
 app.get('/register', (req, res) => {
-    res.render('register');
+    res.render('register', { title: "Home", user: req.user || null, year: new Date().getFullYear() });
 });
 
 app.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', { title: "Home", user: req.user || null, year: new Date().getFullYear() });
 });
 
 app.get('/comments', (req, res) => {
-    res.render('comments');
+    res.render('comments', { title: "Home", user: req.user || null, year: new Date().getFullYear() });
 });
 
 app.get('/comments/new', (req, res) => {
-    res.render('new');
+    res.render('new', { title: "Home", user: req.user || null, year: new Date().getFullYear() });
 });
+
+
+
+//App Post Requests
+app.post('/register', (req, res) => {
+    const {username, password} = req.body;
+
+    if (users[username]){
+    
+    }
+
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+app.post('/logout', (req, res) => {
+
+})
+
+app.post('/comments', (req, res) => {
+
+})
+
 
 
 // Start server
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
-
